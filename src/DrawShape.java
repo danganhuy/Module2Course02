c17e81b72dda80e39ce1a66cc0b75a4c8d38e31e
AnHuy <92672519+danganhuy@users.noreply.github.com>
2024-11-05 15:01:28 +0700

    Exercise 4 finished

---
+++ src/DrawShape.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class DrawShape {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("==== MENU ====");
+        System.out.println("1.Draw the triangle");
+        System.out.println("2.Draw the rectangle");
+        System.out.println("3.Draw the square");
+        System.out.println("4.Draw the diamond");
+        System.out.println("5.Exit");
+        System.out.print("Enter your choice: ");
+
+        int choice = sc.nextInt();
+
+        switch (choice) {
+            case 1:
+                System.out.println("Draw the triangle");
+                System.out.println("******");
+                System.out.println("*****");
+                System.out.println("****");
+                System.out.println("***");
+                System.out.println("**");
+                System.out.println("*");
+                break;
+            case 2:
+                System.out.println("Draw the rectangle");
+                System.out.println("******");
+                System.out.println("******");
+                System.out.println("******");
+                System.out.println("******");
+                System.out.println("******");
+                System.out.println("******");
+                break;
+            case 3:
+                System.out.println("Draw the square");
+                System.out.println("**********");
+                System.out.println("**********");
+                System.out.println("**********");
+                System.out.println("**********");
+                break;
+            case 4:
+                System.out.println("Draw the diamond");
+                System.out.println("      *      ");
+                System.out.println("   *******   ");
+                System.out.println("*************");
+                System.out.println("   *******   ");
+                System.out.println("      *      ");
+            case 0:
+                System.exit(0);
+            default:
+                System.out.println("No choice!");
+        }
+    }
+}
